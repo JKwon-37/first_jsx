4548c8dfa61629fe713594fafda4ad7d87b88959
Jonathan <jonathankwon@yahoo.com>
2022-05-04 15:20:39 -0700

    add folder my-first-jsx

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do</h2>
+      <ol>
+        <li>Learn React</li>
+        <li>Climb Mount Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li> 
+      </ol>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/someClassComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class SomeClassComponent extends Component{
+    render(){
+        return <div>This is a component.</div>
+    }
+}
+
+export default SomeClassComponent;
